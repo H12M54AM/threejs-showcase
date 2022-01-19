6f535aa38033a7e701ee0aa7501428c491fba9e0
Edward Naidoo <securedbread@protonmail.com>
2022-01-18 21:39:23 -0800

    Update to source code and directory
    I have added a pane floor for the cube which the set angles and lighting is now different. I have changed the place where I will be writing code which I really like as it brings more space to the html file

--- source.js
+++ source.js
@@ -1,27 +1,57 @@
-import * as THREE from 'three'
-
 // Add scene
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, -1, 20);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.getElementById('webgl').appendChild(renderer.domElement)
+
+
+// Add Lights
+function addLight(){
+    const skycolor = 0xcfd7f8;
+    const groundcolor = 0xcdb0b0;
+    const intensity = 1;
+    const light = new THREE.HemisphereLight(skycolor, groundcolor, intensity);
+    scene.add(light);
+    // Checks if the light is in the scene, which it is right
+    console.log(light.parent === scene)
+}
 
 // Add Cube
 function addCube() {
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshMaterial( {color: 0x00ff00} );
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
     const cube = new THREE.Mesh(geometry, material);
-
     scene.add(cube);
+    cube.rotation.x += 0.4;
+    cube.rotation.y += 0.4;
+    // Checks if the cube is in the scene, which it is right
+    console.log(cube.parent === scene)
 };
 
-addCube();
-camera.position.x = 5;
+// Add surface mesh
+function addPane() {
+    const geometry = new THREE.BoxGeometry(25, 1, 15);
+    const material = new THREE.MeshBasicMaterial( {color: 0xccccff} );
+    const pane = new THREE.Mesh(geometry, material);
+    scene.add(pane);
+    pane.position.y = -8;
+     // Checks if the cube is in the scene, which it is right
+    console.log(pane.parent === scene)
+}
 
 function animate() {
     requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    renderer.render( scene, camera );
+}   
+
+function Main(){
+    addLight();
+    addCube();
+    addPane();
+    animate();
 }
-animate();
+
+Main();
