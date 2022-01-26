d1b0429af4d841309a406c76f0b057c4d6bad6a3
Edward Naidoo <securedbread@protonmail.com>
2022-01-26 12:19:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://threejs.org/build/three.module.js";
+import * as THREE from "https://threejs.org/build/three.js";
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 
 // Add scene
